e51bbf0d83023616cd0c123f607d7512e1c9b648
Матвей Коновалов <ok__@outlook.com>
2024-04-25 17:33:42 +0300

    Started implementing analysis of embedding quality

---
+++ Analysis.py
@@ -0,0 +1,25 @@
+import ImageWrapper as Img
+from math import floor, log2
+from numpy import int16
+
+def find_max_size(imgPath):
+    img = Img.image_to_array(imgPath)
+    width, height, _ = img.shape
+    cnt = 0
+    for i in range(height - 1):
+        for j in range(0, width - 2, 2):
+            diff = int16(img[i][j][0]) - int16(img[i][j+1][0])
+            n = floor(log2(abs(diff))) if diff != 0 else 0
+            if n < 3: n = 3
+            cnt += n
+
+    return cnt
+
+
+print(find_max_size('input/example1.bmp'))
+
+
+def find_bin_text_length(text):
+    return len(Img.string_to_binary(text))
+
+print(find_bin_text_length(input()))
